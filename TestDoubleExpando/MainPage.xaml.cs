6918b0f67556c53121ab619857f8c047ce23cab1
Amir <amirvenus@me.com>
2020-10-26 20:17:20 +0000

    init

---
+++ TestDoubleExpando/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestDoubleExpando
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            while(true)
+            {
+                ViewModel.Instance.Username = DateTime.Now.ToLongTimeString();
+                
+var nameOfExpandoKey = "Dt";
+ViewModel.Instance.Expando = new System.Dynamic.ExpandoObject();
+(ViewModel.Instance.Expando as IDictionary<string, object>).Add(nameOfExpandoKey, DateTime.Now.ToLongTimeString());
+LabelWithExpando.SetBinding(Label.TextProperty, $"Expando[{nameOfExpandoKey}]");
+var nameOfNestedExpandoKey = "Nested";
+(ViewModel.Instance.Expando as IDictionary<string, object>).Add(nameOfNestedExpandoKey, new ExpandoObject());
+var nameOfSecondNestedKey = "Nested2";
+((ViewModel.Instance.Expando as IDictionary<string, object>)[nameOfNestedExpandoKey] as IDictionary<string, object>).Add(nameOfSecondNestedKey, DateTime.Now.ToLongTimeString());
+LabelWithExpando2.SetBinding(Label.TextProperty, $"Expando[{nameOfExpandoKey}][{nameOfNestedExpandoKey}][{nameOfSecondNestedKey}]");
+await Task.Delay(1500);
+            }
+            
+        }
+    }
+}
---
+++ TestDoubleExpando/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+
+namespace TestDoubleExpando
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private static ViewModel _instance;
+
+        public static ViewModel Instance => _instance ??= new ViewModel();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var propertyChanged = PropertyChanged;
+            propertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private ExpandoObject _expando;
+        public ExpandoObject Expando
+        {
+            get => _expando ??= new ExpandoObject();
+            set
+            {
+                _expando = value;
+                OnPropertyChanged(nameof(Expando));
+            }
+        }
+
+    }
+}
